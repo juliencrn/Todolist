45fdb08f9f929dd6ee2b2192b0dd2869c482c5bb
Julien <juliencaron@protonmail.com>
2021-07-19 14:23:58 +0200

    Add add_task and complete_task methods

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,9 @@
 use chrono::{serde::ts_seconds, DateTime, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::io::{BufReader, Result, Seek, SeekFrom, Error, ErrorKind };
+use std::path::PathBuf;
+use std::OpenOptions;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -15,4 +18,63 @@ impl Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
     }
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    // Open the file.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    // Consume the file's contents as a vector of tasks.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Rewind the file after reading it.
+    file.seek(SeekFrom::Start(0))?;
+
+    // Write the modified task list back into the file.
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    // Open the file.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    // Consume the file's contents as a vector of tasks.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Remove the task.
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid task position"));
+    }
+    tasks.remove(task_position -1));
+
+    // Rewind and truncate the file.
+    file.seek(SeekFrom::Start(0))?;
+    file.set_len(0)?;
+
+    // Write the modified task list back into the file.
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    unimplemented!()
 }
